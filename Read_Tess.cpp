07be47f89ace67aadd9179344b4f9382e49adb5c
TiestoDJ007 <jackxiao007@outlook.com>
2019-10-16 16:42:42 +0800

    reading *.tess program

--- Read_Tess.cpp
+++ Read_Tess.cpp
@@ -16,9 +16,7 @@ int vertex_number;
 int edge_number;
 int face_number;
 int polyhedron_number;
-//int polyhedron_faces_number;
 
-//vector<vector<double >>cell_centroid(cell_number, vector<double>(3));
 vector<string> split(const string &str, const string &pattern);
 
 int main() {
@@ -26,21 +24,14 @@ int main() {
     ifstream read_TESS;
     read_TESS.open("n100-id1.tess", ios::in);
     string line_text;
-    //string line_text_1;
     string cell_text;
     string vertex_text;
     string edge_text;
     string face_text;
-    string face_equation_parameter_text;
-    string face_vertex_text;
     string polyhedron_text;
-    string polyhedron_face_text;
     string skip_line_text;
-    string cell_array;
-    string vertex_array;
     int test = 0;
     while (getline(read_TESS, line_text)) {
-        //getline(read_TESS, line_text);
         if (line_text == " **cell") {
             getline(read_TESS, cell_text);
             seed_number = stoi(cell_text, nullptr, 10);
@@ -71,7 +62,7 @@ int main() {
          << "polyhedron_number is " << polyhedron_number << endl;
     read_TESS.close();
 
-
+    string cell_array;
     vector<vector<double >> cell_centroid(seed_number, vector<double>(3));
     read_TESS.open("n100-id1.tess", ios::in);
     while (!read_TESS.eof()) {
@@ -79,39 +70,114 @@ int main() {
         if (line_text == "  *seed") {
             for (int number_seed = 0; number_seed < seed_number; number_seed++) {
                 getline(read_TESS, cell_array);
-                //cout << cell_array << endl;
                 vector<string> cell_array_split = split(cell_array, " ");
                 double centroid_x = stod(cell_array_split[1]);
                 double centroid_y = stod(cell_array_split[2]);
                 double centroid_z = stod(cell_array_split[3]);
                 cell_centroid[number_seed] = {centroid_x, centroid_y, centroid_z};
-                //cout << " " << centroid_x << " " << centroid_y << " " << centroid_z << endl;
             }
         }
     }
     cout << cell_centroid[1][2] << endl;
     read_TESS.close();
 
+    string vertex_array;
     vector<vector<double >> vertex_cartesian(vertex_number, vector<double>(3));
     read_TESS.open("n100-id1.tess", ios::in);
     while (!read_TESS.eof()) {
-        if (line_text.compare(" **vertex") == 0) {
+        getline(read_TESS, line_text);
+        if (line_text == " **vertex") {
             getline(read_TESS, skip_line_text);
             for (int number_vertex = 0; number_vertex < vertex_number; number_vertex++) {
                 getline(read_TESS, vertex_array);
-                cout << vertex_array << endl;
                 vector<string> vertex_array_split = split(vertex_array, " ");
                 double vertex_x = stod(vertex_array_split[1]);
                 double vertex_y = stod(vertex_array_split[2]);
                 double vertex_z = stod(vertex_array_split[3]);
                 vertex_cartesian[number_vertex] = {vertex_x, vertex_y, vertex_z};
-                cout << vertex_array_split[0] << " " << vertex_array_split[1] << " " << vertex_array_split[4] << endl;
+            }
+            cout << "1" << endl;
+        }
+    }
+    read_TESS.close();
+    cout << vertex_cartesian[1][1] << endl;
+
+
+    string edge_array_text;
+    vector<vector<int >> edge_array(edge_number, vector<int>(2));
+    read_TESS.open("n100-id1.tess", ios::in);
+    while (!read_TESS.eof()) {
+        getline(read_TESS, line_text);
+        if (line_text == " **edge") {
+            getline(read_TESS, skip_line_text);
+            for (int number_edge = 0; number_edge < edge_number; number_edge++) {
+                getline(read_TESS, edge_array_text);
+                vector<string> edge_text_split = split(edge_array_text, " ");
+                int edge_coordination_point_1 = stoi(edge_text_split[1]);
+                int edge_coordination_point_2 = stoi(edge_text_split[2]);
+                edge_array[number_edge] = {edge_coordination_point_1, edge_coordination_point_2};
             }
         }
     }
     read_TESS.close();
+    cout << edge_array[1][1] << endl;
 
 
+    string face_vertex_text;
+    string face_equation_parameter_text;
+    vector<vector<int >> face_vertex_array(face_number, vector<int>(3));
+    vector<vector<double>> face_equation_parameters(face_number, vector<double>(4));
+    read_TESS.open("n100-id1.tess", ios::in);
+    while (!read_TESS.eof()) {
+        getline(read_TESS, line_text);
+        if (line_text == " **face") {
+            getline(read_TESS, skip_line_text);
+            for (int number_face = 0; number_face < face_number; ++number_face) {
+                getline(read_TESS, face_vertex_text);
+                getline(read_TESS, skip_line_text);
+                getline(read_TESS, face_equation_parameter_text);
+                getline(read_TESS, skip_line_text);
+                vector<string> face_vertex_text_split = split(face_vertex_text, " ");
+                int face_vertex_point_1 = stoi(face_vertex_text_split[1]);
+                int face_vertex_point_2 = stoi(face_vertex_text_split[2]);
+                int face_vertex_point_3 = stoi(face_vertex_text_split[3]);
+                face_vertex_array[number_face] = {face_vertex_point_1, face_vertex_point_2, face_vertex_point_3};
+                vector<string> face_eq_para_split = split(face_equation_parameter_text, " ");
+                double face_eq_para_a = stod(face_eq_para_split[0]);
+                double face_eq_para_b = stod(face_eq_para_split[1]);
+                double face_eq_para_c = stod(face_eq_para_split[2]);
+                double face_eq_para_d = stod(face_eq_para_split[3]);
+                face_equation_parameters[number_face] = {face_eq_para_a, face_eq_para_b, face_eq_para_c,
+                                                         face_eq_para_d};
+            }
+        }
+    }
+    read_TESS.close();
+    cout << face_vertex_array[1][1] << " " << face_equation_parameters[1][1] << endl;
+
+    string polyhedron_face_text;
+    vector<vector<int >> polyhedron_faces;
+    read_TESS.open("n100-id1.tess", ios::in);
+    while (!read_TESS.eof()) {
+        getline(read_TESS, line_text);
+        if (line_text == " **polyhedron") {
+            getline(read_TESS, skip_line_text);
+            for (int number_polyhedron = 0; number_polyhedron < polyhedron_number; ++number_polyhedron) {
+                getline(read_TESS, polyhedron_face_text);
+                vector<string> polyhedron_text_split = split(polyhedron_face_text, " ");
+                vector<int> face_list;
+                int polyhedron_faces_number = polyhedron_text_split.size();
+                for (int number_polyhedron_face = 1;
+                     number_polyhedron_face < polyhedron_faces_number; ++number_polyhedron_face) {
+                    int face_list_element = abs(stoi(polyhedron_text_split[number_polyhedron_face]));
+                    face_list.push_back(face_list_element);
+                }
+                polyhedron_faces.push_back(face_list);
+            }
+        }
+    }
+    read_TESS.close();
+    cout << polyhedron_faces[1][1] << endl;
     return 0;
 
 }
